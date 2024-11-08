3dd0264441692be7f24238319711eb6fc52c6eb6
hiren1137 <digihiren@gmail.com>
2024-11-09 01:06:25 +0530

    Add sitemap generation configuration

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.spendelonmuskmoney.org/', // Replace with your site's URL
+    generateRobotsTxt: true, // (Optional) Generate `robots.txt` file
+    sitemapSize: 5000, // Limit sitemap size (optional)
+  };
+  
